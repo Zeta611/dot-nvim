47977f3c850ce42f037e8a5b0ea6ba9dbc0d9548
Jay Lee <jaeho.lee@snu.ac.kr>
2024-12-11 19:56:54 +0900

    :wrench: Filter minor warnings in vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,5 +3,6 @@ return {
   lazy = false,
   init = function()
     vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_quickfix_ignore_filters = { "LaTeX Font Warning: Size substitutions" }
   end,
 }
